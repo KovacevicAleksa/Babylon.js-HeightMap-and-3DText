2af94765013547575c01ad68bf3a7668447ddd43
Aleksa Kovacevic <48535139+KovacevicAleksa@users.noreply.github.com>
2023-07-19 22:41:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,21 @@ const engine = new BABYLON.Engine(canvas);
 const createScene = async () => {
   const scene = new BABYLON.Scene(engine);
 
-  scene.createDefaultCameraOrLight(true, false, true);
+  scene.createDefaultLight();
+  //const camera = new BABYLON.UniversalCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+  const camera = new BABYLON.ArcRotateCamera('camera',0,0,10, new BABYLON.Vector3(0,0,0 ), scene);
+  camera.attachControl(true);
+  //camera.inputs.addMouseWheel();
+  //camera.setTarget(BABYLON.Vector3.Zero());
 
+  camera.setPosition(new BABYLON.Vector3(0, 0, -20));
+  camera.lowerBetaLimit = Math.PI /4;
+  camera.upperBetaLimit = Math.PI /2;
+
+  camera.lowerRadiusLimit = 20;
+  camera.upperRadiusLimit = 50;
+
+/*
   const groundFromHM = BABYLON.MeshBuilder.CreateGroundFromHeightMap('ground', '/heightmap.png', {
     width: 10,
     height: 10,
@@ -18,7 +31,14 @@ const createScene = async () => {
 
   groundFromHM.material = new BABYLON.StandardMaterial();
   groundFromHM.material.wireframe = true;
+  */
 
+  const fontData = await (await fetch('/Montserrat_Regular.json')).json();
+  const text = BABYLON.MeshBuilder.CreateText('','Test',fontData, {
+    size:2,
+    depth:0.1,
+    resolution:64
+  });
 
   return scene;
 }
